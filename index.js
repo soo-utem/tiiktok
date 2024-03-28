dadc30dba4959e4a0f61da13c4c942532faec714
soo-utem <soo@utem.edu.my>
2024-03-28 15:19:23 +0800

    mongo basic functions

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const port = process.env.PORT || 3000;
 app.use(express.json())
 
 app.get('/', (req, res) => {
-   res.send('BERR 2434 Database and Cloud')
+  res.send('BERR 2434 Database and Cloud')
 })
 
 app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://soo:passwordbaharu@cluster0.nehnjjb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,12 +28,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log('Connected successfully to MongoDB')
+
+    // insert a user document to the users collection
+    let result = await client.db('maybank2u').collection('students').find().toArray();
+    
+    console.log(result)
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
